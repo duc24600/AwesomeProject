d19944beb1a9e43afbf10ed04b982010921f1966
duc24600 <duc24600@gmail.com>
2023-04-05 11:29:42 +0700

    commit demo action update on device

--- App.jsx
+++ App.jsx
@@ -6,38 +6,87 @@
  * @format
  */
 
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import {
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View
 } from "react-native"
+import abab from "abab"
+
 
 function App() {
-  const [state, setState] = useState({ count: 0 });
+  const [message, setMessage] = useState('');
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const updateData = async () => {
+    let data = JSON.stringify({
+      Device : {
+        DisplayName : name
+      }
+    });
+    let credentials = abab.btoa("hpt.admin:hpthnadmin");
+    let auth = { 
+      "Authorization": `Basic ${credentials}`,
+      "Content-Type" : "application/json",
+      "Content-Length" : `${data.length}`,
+      "Host" : "10.4.18.52:13081"
+    };
+    try {
+      const res = await fetch(`http://10.4.18.52:13081/SM/9/rest/devices/${id}`,
+        {
+          headers: auth,
+          method: 'PUT',
+          body: data
+        });
+      const json = await res.json();
+      setMessage(json.Messages[0]);
+    } catch (error) {
+      console.log(error);
+      setMessage("Update failed!");
+    }
+  };
 
   onPress = () => {
-    setState({
-      count: state.count + 1,
-    });
+    updateData();
   };
+
   return (
     <View style={styles.container}>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Input Logical Name"
+          value={id}
+          onChangeText={setId} />
+        <TextInput
+          style={styles.input}
+          placeholder="Input name"
+          value={name}
+          onChangeText={setName} />
+      </View>
       <TouchableOpacity style={styles.button} onPress={this.onPress}>
-        <Text>Click</Text>
+        <Text>Update</Text>
       </TouchableOpacity>
       <View>
-        <Text>You clicked {state.count} times</Text>
+        <Text>{message}</Text>
       </View>
     </View>
   )
 }
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "stretch"
   },
   button: {
     alignItems: "center",
